306d2833ecd1352ef8299d9e7316e579dfc460a9
carlos-eugenio <carlos-eugenio@live.com>
2020-04-10 22:44:52 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        const cardImg = card.getElementsByClassName("card__image-container")[0].querySelector("img").src
+        const cardTitle = card.getElementsByClassName("card__title")[0].querySelector("p").innerHTML
+        const cardAuthor = card.getElementsByClassName("card__author")[0].querySelector("p").innerHTML
+        modalOverlay.getElementsByClassName("modal__image-container")[0].querySelector("img").src = cardImg
+        modalOverlay.getElementsByClassName("modal__title")[0].querySelector("p").innerHTML = cardTitle
+        modalOverlay.getElementsByClassName("modal__author")[0].querySelector("p").innerHTML = cardAuthor
+        modalOverlay.classList.add('active')
+    })
+}
+
+document.querySelector(".modal__close").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+    //modalOverlay.querySelector("iframe").src = ""
+})
