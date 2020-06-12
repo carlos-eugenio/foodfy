cf836b2a5d02dffecdbf8b638a5cccea4dbef722
carlos-eugenio <carlos-eugenio@live.com>
2020-06-11 21:39:17 -0300

    chore: add function to show/hide recipe details in scripts.js, page recipe-details.njk completed

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,25 @@
 const cards = document.querySelectorAll('.card')
+const recipe_description = document.querySelectorAll('.recipe-description')
 
 for (let card of cards) {
     card.addEventListener("click", function(){
         const recipeId = card.getAttribute("id")
         window.location.href = `/receitas/${recipeId}`
+    })
+}
+
+for (let recipe of recipe_description) {
+    const recipe_nav = recipe.querySelector('a')
+    recipe_nav.addEventListener("click", function(){        
+        const recipe_article = recipe.querySelector('article')
+    
+        if (recipe_article.classList.contains('active')){
+            recipe_nav.innerHTML = 'MOSTRAR'
+            recipe_article.classList.remove('active')  
+        }
+        else {            
+            recipe_nav.innerHTML = 'ESCONDER'
+            recipe_article.classList.add('active')
+        }
     })
 }
